e11b7e8470f975aa9a759d0bf60a76add0f6a3ab
Géry Thrasibule <gery@thrasibule.mx>
2019-06-06 19:06:46 +0200

    First and Last Commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Créer une liste comprenant des entiers de 1 à 100
+
+        /*
+            Boucler sur cette liste
+            - Afficher "Fizz" pour un nombre divisible par 3
+            - Afficher "Buzz" pour un nombre divisible par 5
+            - Afficher le nombre sinon
+         */
+    }
+}
